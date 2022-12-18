e19ea4ea8b0b7b2c5ccad75d862d759e2fe73625
sunghyeok <glxm06052@gmail.com>
2022-12-18 21:21:02 +0900

    변경

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,6 +19,8 @@ public class JpaMain {
             Member findMember = em.find(Member.class, 1L);
             findMember.setName("HelloJPA");
 
+            em.flush();
+            em.clear();
             tx.commit();
         }catch (Exception e){
             tx.rollback();
